54bdf6f99524f665f1a6e2cf7e0b09b9b009141d
oibaf06 <91890269+oibaf06@users.noreply.github.com>
2022-03-24 23:06:36 +0100

    3

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
+    port: 9876 || process.env.PORT,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
